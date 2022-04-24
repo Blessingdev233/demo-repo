ffa2efff64b9c4ba30f11fe1da2ffe4a19b0e3ce
Blessingdev233 <blessing.lartey@gmail.com>
2022-04-24 11:11:26 +0000

    not done yet

---
+++ BMI-Calculator/bmi.js
@@ -0,0 +1,24 @@
+var age = document.getElementById("age");
+var height = document.getElementById("height");
+var weight = document.getElementById("weight");
+var male = document.getElementById("m");
+var female = document.getElementById("f");
+var form = document.getElementById("form");
+
+function validateForm(){
+    if(age.value=='' || height.value=='' || weight.value=='' || (male.checked==false && female.checked==false)){
+        alert("All fields are required!");
+        document.getElementById("submit").addEventListener("click", countBmi);
+    
+    }else {  countBmi(); }
+} document.getElementById("submit").addEventListener("click", validateForm);
+
+function countBmi(){
+    var p = [age.value, height.value, weight.vale];
+    if(male.checked){
+        p.push("male");
+    }else if(female.checked){
+        p.push("female");
+    }
+    
+}
