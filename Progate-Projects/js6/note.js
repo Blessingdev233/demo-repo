5aafc8db866eda884e99907108efc04f610536b2
Blessingdev233 <blessing.lartey@gmail.com>
2022-06-27 08:26:53 +0000

    using this

--- Progate-Projects/js6/note.js
+++ Progate-Projects/js6/note.js
@@ -10,3 +10,24 @@ class Animal {
 
 const animal = new Animal();
 
+//using 'this'
+
+
+class Animal {
+  constructor() {
+    // Assign the string value "Leo" to the name property
+      this.name = "Leo";
+    
+    // Assign the value "3" to the age property
+    this.age = 3;
+  }
+}
+
+const animal = new Animal();
+
+// Print 「Name: ____」
+console.log(`Name: ${animal.name}`);
+
+// Print 「Age: ____」
+console.log(`Age: ${animal.age}`);
+
