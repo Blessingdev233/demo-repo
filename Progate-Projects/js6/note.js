9decec5d6ab89e4fb42f37016af62e346c4716cb
Blessingdev233 <blessing.lartey@gmail.com>
2022-06-27 10:34:14 +0000

    overriding

--- Progate-Projects/js6/note.js
+++ Progate-Projects/js6/note.js
@@ -82,5 +82,56 @@ animal.info();
 
 
 //// Define the Dog class to inherit from the Animal class
+
 class Dog extends Animal {
 }
+
+
+
+//overiding
+
+
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  greet() {
+    console.log("Hello");
+  }
+  
+  info() {
+    this.greet();
+    console.log(`My name is ${this.name}`);
+    console.log(`I'm ${this.age} years old`);
+  }
+}
+
+class Dog extends Animal {
+  // Add the constructor
+  constructor(name, age, breed) {
+    super(name, age);
+    this.breed = breed;
+  }
+  
+  info() {
+    this.greet();
+    console.log(`My name is ${this.name}`);
+    // Print 「I am a ____」
+    console.log(`I am a ${this.breed}`);
+    
+    console.log(`I'm ${this.age} years old`);
+    const humanAge = this.getHumanAge();
+    console.log(`I am ${humanAge} years old in human years`);
+  }
+  
+  getHumanAge() {
+    return this.age * 7;
+  }
+}
+
+// Pass the string "Chihuahua" as an argument
+const dog = new Dog("Leo", 4, "Chihuahua");
+dog.info();
+
