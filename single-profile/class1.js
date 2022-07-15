9f649f8481af41e7a66195243ced0029b8ebbf5a
Blessingdev233 <blessing.lartey@gmail.com>
2022-07-15 14:02:42 +0000

    classes and default parameters

--- single-profile/class1.js
+++ single-profile/class1.js
@@ -52,3 +52,26 @@ var highSpeed1 = new HighSpeedTrain(200, false, 'green', false);
 train5.toggleLights(); // undefined
 train5.lightsStatus(); // Lights on? true
 highSpeed1.toggleLights(); // Lights on? true, Lights are 100% operational.
+
+
+
+
+
+class WithDefaultParams {
+    constructor(num1 = 1, num2 = 2, num3 = 3, string1 = "Result:", bool1 = true) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+        this.string1 = string1;
+        this.bool1 = bool1;
+    }
+    calculate() {
+        if(this.bool1) {
+            console.log(this.string1, this.num1 + this.num2 + this.num3);
+            return;
+        }
+        return "The value of bool1 is incorrect"
+    }
+}
+var better = new WithDefaultParams();
+better.calculate(); // Result: 6
